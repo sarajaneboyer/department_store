63b919f5a61c2c8bdf926833ad3053e04cd9d15f
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-20 15:14:28 -0700

    index working link_to #new working

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,37 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
+
   def index
+    @departments = Department.all
   end
 
   def show
   end
 
   def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to departments_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+
+  private
+    def set_department
+       @department = Department.find(params[:id])
+    end
+
+    def department_params
+       params.require(:department).permit(:name)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+# require 'faker'
 
+# 10.times do
+#   department = Department.create(name: Faker::Company.industry)
+# end
 
-10.times do
-  department = Department.create(department: Faker::Company.industry)
-end
+Department.create(name: 'Clothing')
+Department.create(name: 'Furniture')
+Department.create(name: 'Home Appliances')
+Department.create(name: 'Toys')
+Department.create(name: 'Cosmetics')
+Department.create(name: 'Houseware')
+Department.create(name: 'Sporting Goods')
+Department.create(name: 'Hardware & Tools')
