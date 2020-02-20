ef594682bbaa8aa6d7aa70e279dab8553ab908c9
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-20 15:37:51 -0700

    created items model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/edit'
   root  'departments#index'
   resources :departments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_213917) do
+ActiveRecord::Schema.define(version: 2020_02_20_223440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
